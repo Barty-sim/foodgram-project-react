8a5b3badfb0f81617a8b7d0a4a5b377509fa4484
Сергей Терещенко <barty.sim1@yandex.ru>
2023-05-02 22:07:01 +0300

    pp

--- backend/api/views.py
+++ backend/api/views.py
@@ -160,34 +160,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     @action(
         detail=False, methods=['get'], permission_classes=[IsAuthenticated])
-    def download_shopping_cart(self, request):
-        user = request.user
-        if not user.shopping_cart.exists():
-            return Response(status=HTTP_400_BAD_REQUEST)
-
-        ingredients = IngredientAmount.objects.filter(
-            recipe__shopping_cart__user=request.user
-        ).values(
-            'ingredient__name',
-            'ingredient__measurement_unit'
-        ).order_by('ingredient__name'
-        ).annotate(amount=Sum('amount'))
-
-        today = datetime.today()
-        shopping_list = (
-            f'Список покупок для: {user.get_full_name()}\n\n'
-            f'Дата: {today:%Y-%m-%d}\n\n'
-        )
-        shopping_list += '\n'.join([
-            f'- {ingredient["ingredient__name"]} '
-            f'({ingredient["ingredient__measurement_unit"]})'
-            f' - {ingredient["amount"]}'
-            for ingredient in ingredients
-        ])
-        shopping_list += f'\n\nFoodgram ({today:%Y})'
-
-        filename = f'{user.username}_shopping_list.txt'
-        response = HttpResponse(shopping_list, content_type='text/plain')
-        response['Content-Disposition'] = f'attachment; filename={filename}'
-
+    def download_shopping_cart(self, request): 
+        ingredients = IngredientAmount.objects.filter( 
+            recipe__cart__user=request.user).values(
+                'ingredients__name', 
+                'ingredients__measurement_unit').order_by(
+                'ingredients__name').annotate(total=Sum('amount')) 
+        shopping_cart = '\n'.join([ 
+            f'{ingredient["ingredients__name"]} - {ingredient["total"]} ' 
+            f'{ingredient["ingredients__measurement_unit"]}' 
+            for ingredient in ingredients 
+        ]) 
+        filename = 'shopping_cart.txt' 
+        response = HttpResponse(shopping_cart, content_type='text/plain') 
+        response['Content-Disposition'] = f'attachment; filename={filename}' 
         return response
